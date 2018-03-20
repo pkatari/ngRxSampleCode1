8933b3c987b32a00bfa541a17891012db5d10659
Priyanka Kataria <pkataria2@sapient.com>
2018-03-20 14:44:06 +0530

    codeee.

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { CurrenciesUpdateAction } from './actions/currency';
+import { Currency } from './models/currency';
+import { AmountChangeAction } from './actions/amount';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromRoot from './reducers';
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+    public amount$: Observable<number>;
+    public currencyRates$: Observable<Currency[]>;
+
+    constructor(public store: Store<fromRoot.State>) {
+        this.amount$ = store.select(fromRoot.getAmountState);
+        this.currencyRates$ = store.select(fromRoot.getCurrnecyRates);
+    }
+
+    ngOnInit() {
+        this.store.dispatch(new CurrenciesUpdateAction());
+    }
+
+    onAmountChange(amount: string) {
+        const number = parseFloat(amount);
+        if (!isNaN(number)) this.store.dispatch(new AmountChangeAction(number));
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { CurrencyService } from './services/currency.service';
+import { CurrencyEffects } from './effects/currencyEffects';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './reducers/index';
+import { HttpClientModule } from '@angular/common/http';
+
+import { EffectsModule } from '@ngrx/effects';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([CurrencyEffects])
+    ],
+    providers: [CurrencyService],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
